72131749aae7c16363c144e95326433df4ce7884
Zephyr Guo <zephyr@atomi.ai>
2024-05-09 20:17:43 -0700

    Moved main.rs to hx711_exp.rs, and confirmed the correctness of HX711.

---
+++ examples/blinky.rs
@@ -0,0 +1,41 @@
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to the pin assigned to `led`. (GPIO0)
+
+//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl,
+    delay::Delay,
+    gpio::IO,
+    peripherals::Peripherals,
+    prelude::*,
+};
+
+#[entry]
+fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    // Set GPIO0 as an output, and set its state high initially.
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio8.into_push_pull_output();
+
+    led.set_high();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let delay = Delay::new(&clocks);
+
+    loop {
+        log::info!("Hello world!");
+        led.toggle();
+        delay.delay_millis(500);
+    }
+}
